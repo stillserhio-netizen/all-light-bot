b563bdf88468d968df3409359a559e3b6a1147e9
stillserhio-netizen <stillserhio@gmail.com>
2026-03-03 20:49:02 +0200

    Implement basic Telegram bot functionality

---
+++ bot.py
@@ -0,0 +1,16 @@
+import requests
+
+BOT_TOKEN = "8531283640:AAGcDueeQqu-nXZ8aYrBT7lh8lABOWi9Crs"
+CHAT_ID = "-1003802691352"
+
+def main():
+    requests.post(
+        f"https://api.telegram.org/bot8531283640:AAGcDueeQqu-nXZ8aYrBT7lh8lABOWi9Crs/sendMessage",
+        data={
+            "chat_id": CHAT_ID,
+            "text": "Бот запущено з GitHub ✅"
+        }
+    )
+
+if name == "main":
+    main()
